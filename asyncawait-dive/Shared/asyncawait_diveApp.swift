ba75d748637dbc934c1f20bfa62cbecc26beccbc
Henry Zhang <henryzh@microsoft.com>
2022-03-04 09:42:31 -0800

    bootstrap

---
+++ asyncawait-dive/Shared/asyncawait_diveApp.swift
@@ -0,0 +1,17 @@
+//
+//  asyncawait_diveApp.swift
+//  Shared
+//
+//  Created by Hank Zhang on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct asyncawait_diveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
